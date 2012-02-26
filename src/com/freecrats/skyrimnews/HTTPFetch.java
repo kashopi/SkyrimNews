57f3698be8a53568665d825ed2842d070686f30e
Kashopi <kashopi@gmail.com>
2012-02-26 10:07:23 +0100

    Funcionando la parte de HTTP ...

---
+++ src/com/freecrats/skyrimnews/HTTPFetch.java
@@ -0,0 +1,37 @@
+package com.freecrats.skyrimnews;
+import java.io.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class HTTPFetch {
+	public static InputStream getInputStreamFromURL(String url){
+		InputStream contentStream = null;
+		try{
+			HttpClient httpc=new DefaultHttpClient();
+			HttpResponse response = httpc.execute(new HttpGet("http://www.bethblog.com"));
+			contentStream = response.getEntity().getContent();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return(contentStream);
+	}
+	
+	public static String getStringFromURL(String url) throws UnsupportedEncodingException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(getInputStreamFromURL(url)));
+		StringBuffer sb = new StringBuffer();
+		
+		try{
+			String line = null;
+			while((line = br.readLine())!=null){
+				sb.append(line);
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return(sb.toString());
+	}
+	
+}
--- src/com/freecrats/skyrimnews/SkyrimNewsActivity.java
+++ src/com/freecrats/skyrimnews/SkyrimNewsActivity.java
@@ -1,7 +1,12 @@
 package com.freecrats.skyrimnews;
 
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SkyrimNewsActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,20 @@ public class SkyrimNewsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        try {
+			String contenido = HTTPFetch.getStringFromURL("http://www.bethblog.com");
+			Pattern pattern = Pattern.compile("<title>([^<]+)</title>");
+			Matcher matcher = pattern.matcher(contenido);
+			if(matcher.find()){
+				String titulo = matcher.group();
+				Log.d("MATCH",titulo);
+			}
+			
+			Log.d("HTTP",contenido);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
