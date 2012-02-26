068fef93e745cfe0bf4b2a26f3efc2a30f730791
Kashopi <kashopi@gmail.com>
2012-02-26 09:25:52 +0100

    Inicial

---
+++ src/com/freecrats/skyrimnews/SkyrimNewsActivity.java
@@ -0,0 +1,13 @@
+package com.freecrats.skyrimnews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SkyrimNewsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
